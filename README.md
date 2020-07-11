aa1d5bfb60ae1941b1392dae1769565d5a6440f5
ruichima <ruichima@outlook.com>
2020-07-11 14:09:28 +0800

    init

---
+++ README.md
@@ -0,0 +1,24 @@
+# VSCode extension Markdown Lower Headers Level
+This VSCode extension is developed to lower the level of all the titles of the selected part of a Markdown file.
+
+I used [Academic](https://github.com/gcushen/hugo-academic) to build my personal website. However, I encountered difficulties when adding the Markdown file in the Courses section: the TOC in this section uses the second level headers of the Markdown file to construct its first level headings, and so on for other headers. To deal with this problem, I developed this VSCode extension.
+
+## Features
+
+
+## Install & Run
+
+
+## Resource code
+
+https://github.com/ruichima/markdown-lower-headers-levels
+
+## Release Notes
+
+### 1.0.0
+
+Initial release.
+
+## License
+
+MIT
---
+++ extension.js
@@ -0,0 +1,100 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require('vscode');
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+
+
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+
+
+// 给待匹配的文本中所有匹配正则表达式的字符串的前面或者后面加字符串的函数
+// re：正则表达式， rp1：要加在前面的字符串， rp2：要加在后面的字符串， str：待匹配的文本
+function RegInsertStr(re, rp1, rp2, str) {
+    var arr = str.match(re);
+    var ret = "";
+    var rpt = "";
+    var re2 = null;
+
+    if (!arr) return str;
+
+    for (var i = 0; i < arr.length; i++) {
+        ret = arr[i];
+        rpt = rp1 + arr[i] + rp2;
+        re2 = new RegExp(ret, "ig");
+        str = str.replace(re2, rpt);
+    }
+    return str;
+}
+
+function activate(context) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	// console.log('Congratulations, your extension "helloworld" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with  registerCommand
+	// The commandId parameter must match the command field in package.json
+	let disposable = vscode.commands.registerCommand('extension.markdown_lower_headers_levels', function () {
+		// The code you place here will be executed every time your command is executed
+
+		let activeTextEditor = vscode.window.activeTextEditor;
+		let activeDocument = activeTextEditor.document;
+
+		// 1. 获取所有选中行信息
+		let selection = vscode.window.activeTextEditor.selection;
+
+		// 起止行行号
+		let startLine = selection.start.line;
+		let endLine = selection.end.line;
+
+		// 2. 获取每行的文本信息，是标题的存入数组
+		let titleArr = [];                                                           // 选中行信息缓存数组
+		var re = new RegExp('^#+.*', "gm");
+		// var re = new RegExp('^[^\n\r]\s*#+.*', "gm");
+		// var re = new RegExp('(^\\s*\\#)\\s*((\\d+\\.)+)\\s+', "g");
+		for(let i = startLine; i <= endLine; i++) {
+			let curLineText = activeDocument.lineAt(i).text;                           // 当前行文本内容
+			let curLine_plus = RegInsertStr(re, "#", "", curLineText);               // 在标题前面加#
+
+			titleArr.push({
+				line: i,
+				lineLength: curLineText.length,
+				curLineText: curLineText,
+				curLine_plus: curLine_plus
+			});
+		}
+		
+		
+		// 3. 把前面加了#的新标题提取出来
+		let newText = titleArr.map(item => {
+			return item.curLine_plus;
+		});
+		//vscode.Range4个参数的含义：startLine, startCharacter, endLine, endCharacter
+		let replaceRange = new vscode.Range(startLine, 0, endLine, titleArr[titleArr.length - 1].lineLength);
+		
+		// 4. 调用编辑接口，用前面加了#的新标题，替换原来的标题
+		activeTextEditor.edit((TextEditorEdit) => {
+			TextEditorEdit.replace(replaceRange, newText.join('\n'));
+		});
+
+		// Display a message box to the user
+		vscode.window.showInformationMessage('success!');
+	});
+
+	context.subscriptions.push(disposable);
+}
+exports.activate = activate;
+
+// this method is called when your extension is deactivated
+function deactivate() {}
+
+module.exports = {
+	activate,
+	deactivate
+}
